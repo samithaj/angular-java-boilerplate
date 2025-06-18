8a1ec86171efc9bb0f8f919e46afe018389e49df
Samitha Jayaweera <samithajayaweera@gmail.com>
2025-06-18 15:51:34 +0530

    fix main layout nav bar and form field

--- frontend-angular/src/app/core/layout/main-layout.component.ts
+++ frontend-angular/src/app/core/layout/main-layout.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, MatToolbarModule, MatTabsModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatTabsModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
